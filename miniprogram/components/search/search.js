77b65adbe51e756cb065b1a64593d26f8e8c486a
LuoJinAn <1017386624@qq.com>
2020-05-12 23:05:12 +0800

    发现页面头部搜索组件，页面传递样式处理组件样式隔离

---
+++ miniprogram/components/search/search.js
@@ -0,0 +1,27 @@
+// components/search/search.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    placeholder:{
+      type:String,
+      value:'请输入关键词'
+    }
+  },
+  // 接收页面传递进来的样式，不可在组件中再修改传入的class样式。[组件的样式隔离]()
+  externalClasses:['iconfont-class','icon-sousuo-class'],
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
