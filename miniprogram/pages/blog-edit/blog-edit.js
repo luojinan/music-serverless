aa1c6579bc35c6153d2b8f3012a017c3c60a65f2
LuoJinAn <1017386624@qq.com>
2020-05-17 14:07:57 +0800

    前端上传云存储，并直接操作数据库新增blog表

--- miniprogram/pages/blog-edit/blog-edit.js
+++ miniprogram/pages/blog-edit/blog-edit.js
@@ -1,61 +1,129 @@
 // miniprogram/pages/blog-edit/blog-edit.js
-import { promisify } from 'miniprogram-api-promise';
+import {
+  promisify
+} from 'miniprogram-api-promise';
 const chooseImage = promisify(wx.chooseImage)
 const MAX_WORDS_NUM = 140
 const MAX_IMAGE_NUM = 140
+const db = wx.cloud.database()
+let msgText = ''
+let fileIds = []
+let userInfo = {}
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    images:[],
-    wordsNum:0,
-    footerBottom:0
+    images: [],
+    wordsNum: 0,
+    footerBottom: 0
   },
-  async onChooseImage(){
+  async uploadImg(){
+    let promiseList = []
+    for (const item of this.data.images) {
+      let p = new Promise((resolve, reject) => {
+        // 文件拓展名
+        const suffix = /\.\w+$/.exec(item)[0]
+        wx.cloud.uploadFile({
+          // 指定上传到的云路径
+          cloudPath: `blog/${Date.now()}-${Math.random()*1000000}${suffix}`,
+          // 指定要上传的文件的小程序临时文件路径
+          filePath: item,
+          success: res => {
+            fileIds.push(res.fileID)
+            resolve()
+          },
+          fail: err => {
+            console.log('上传失败', err);
+            reject()
+          }
+        })
+      })
+      promiseList.push(p)
+    }
+    const allRes = await Promise.all(promiseList).catch(err=>{
+      console.log('catch到promise all的错误，不会中断逻辑，可以return标识');
+      return false
+    })
+    if(!allRes) {
+      return false
+    }
+    return true
+  },
+  async addBlog(){
+    const params = {
+      data: {
+        msgText,
+        ...userInfo,
+        imgs: fileIds,
+        createTime: db.serverDate()
+      }
+    }
+    // 操作数据库，blog表
+    await db.collection('blog').add(params)
+    wx.showToast({title: '发布成功'})
+    wx.navigateBack()
+  },
+  async onSubmit() {
+    if (!msgText) {
+      return wx.showToast({
+        title: '请先填写分享内容',
+        icon: 'none'
+      })
+    }
+    wx.showLoading({
+      title: '发布中',
+      mask: true
+    });
+    const uploadRes = await this.uploadImg()
+    if(!uploadRes) return wx.showToast({title:'上传失败',icon:'none'})
+    this.addBlog()
+  },
+  async onChooseImage() {
     const res = await chooseImage({
-      count: MAX_IMAGE_NUM-this.data.images.length,
+      count: MAX_IMAGE_NUM - this.data.images.length,
       sizeType: ['original', 'compressed'],
       sourceType: ['album', 'camera']
     })
     this.setData({
-      images:[...this.data.images,...res.tempFilePaths]
+      images: [...this.data.images, ...res.tempFilePaths]
     })
   },
-  onPreviewImage(e){
+  onPreviewImage(e) {
     console.log(e.target);
     wx.previewImage({
       current: e.target.dataset.imgurl,
       urls: this.data.images
     })
   },
-  onDelete(e){
-    this.data.images.splice(e.target.dataset.index,1)
+  onDelete(e) {
+    this.data.images.splice(e.target.dataset.index, 1)
     this.setData({
-      images:this.data.images
+      images: this.data.images
     })
   },
-  onInput(e){
+  onInput(e) {
     const text = e.detail.value
-    if(text.length>=MAX_WORDS_NUM){
+    if (text.length >= MAX_WORDS_NUM) {
       this.setData({
-        wordsNum:`最大字数为${MAX_WORDS_NUM}`
+        wordsNum: `最大字数为${MAX_WORDS_NUM}`
       })
-    }else{
+    } else {
+      msgText = text
       this.setData({
-        wordsNum:text.length
+        wordsNum: text.length
       })
     }
   },
-  onFocus(e){
+  onFocus(e) {
     this.setData({
-      footerBottom:e.detail.height  // 键盘高度
+      footerBottom: e.detail.height // 键盘高度
     })
   },
-  onBlur(){
+  onBlur() {
     this.setData({
-      footerBottom:0
+      footerBottom: 0
     })
   },
   /**
@@ -63,6 +131,7 @@ Page({
    */
   onLoad: function (options) {
     console.log(options);
+    userInfo = options
   },
 
   /**
